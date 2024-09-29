04acd3bb214e16d8b3d9aa96e30b21782310e40b
Rocky <macbook@Macbooks-MacBook-Pro.local>
2024-09-29 19:18:50 +0700

    update database dsn from env

--- config/config.go
+++ config/config.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -11,6 +12,10 @@ import (
 var DB *gorm.DB
 
 func ConnectDB() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	dsn := os.Getenv("DB_CONN")
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
